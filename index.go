51ee872826536d391f8101fd06137cc3df31e6f9
atippaz <nasakun13201@gmail.com>
2024-05-23 14:33:01 +0700

    update

--- index.go
+++ index.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	client "test/grpc/client"
 	server "test/grpc/server"
 	two "test/two"
 )
 
 func main(){
+	fmt.Println("1. \n")
 	two.Test()
+	fmt.Println("\n")
+	fmt.Println("2. \n")
 	server.StartServer()
 	client.StartSendRequest()
+	fmt.Println("\n")
+
 }
